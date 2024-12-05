85fa7950b0d80e99ced8f74a6670b23021a7ac52
AKAUSAR03 <sabeera1957@gmail.com>
2024-12-05 09:48:39 +0530

    Add files via upload

---
+++ lab program 4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+	int i,j,n;
+	printf("Input number of rows:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	/*print blank space*/
+	for (j=1;j<=n-i;j++)
+	printf(" ");
+	/*Numbers in ascending order*/
+	 for (j=1;j<=i;j++)
+	 printf("%d",j);
+	 /*reverse order*/
+	 for(j=i-1;j>=1;j--)
+	 printf("%d",j);
+	 printf("\n");
+	}
+	
+}
